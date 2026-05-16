76bb890bdbd5bd86dec027ec9f9d9a31d67a2039
kamilPiHub <pizon792@gmail.com>
2026-05-16 16:13:40 +0200

    fixing vars

--- terraform/modules/dns_domain/main.tf
+++ terraform/modules/dns_domain/main.tf
@@ -1,15 +1,12 @@
-# Sprawdzamy czy domena została realnie zmieniona z domyślnej
 locals {
   has_real_domain = var.domain_name != "sendr.com" && var.domain_name != ""
 }
 
-# Pobieramy informację o domenie, tylko jeśli zmieniono wartość z domyślnej
 data "digitalocean_domain" "main" {
   count = local.has_real_domain ? 1 : 0
   name  = var.domain_name
 }
 
-# Tworzymy subdomenę dla środowiska (np. dev.twojadomena.com)
 resource "digitalocean_record" "ingress" {
   count  = local.has_real_domain && var.ingress_ip != "" ? 1 : 0
   domain = data.digitalocean_domain.main[0].name
@@ -20,7 +17,6 @@ resource "digitalocean_record" "ingress" {
   ttl   = 300
 }
 
-# Zabezpieczenie dla prod (tworzymy też rekord www)
 resource "digitalocean_record" "www_prod" {
   count  = local.has_real_domain && var.environment == "prod" && var.ingress_ip != "" ? 1 : 0
   domain = data.digitalocean_domain.main[0].name
