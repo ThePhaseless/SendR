13abe3676bd8ca2448594f0228f782ad4e103dc4
ThePhaseless <kukubaorch@gmail.com>
2026-05-18 22:49:38 +0000

    Default frontend nginx DNS resolver

--- frontend/docker-entrypoint.sh
+++ frontend/docker-entrypoint.sh
@@ -1,2 +1,8 @@
 #!/bin/sh
+
+if [ -z "${DNS_RESOLVER:-}" ]; then
+  DNS_RESOLVER=$(awk '/^nameserver / { print $2; exit }' /etc/resolv.conf)
+  export DNS_RESOLVER
+fi
+
 exec /docker-entrypoint.sh "$@"
